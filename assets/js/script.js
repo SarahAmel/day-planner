dd5ba7e85db6bf5684d1021265904ee6e3d8a206
sarah attar <sarahattar@sarahs-MacBook-Pro.local>
2022-05-16 17:38:00 -0400

    structured the folders and files, edited the html to work with the css, started some js

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+// var hour9El = document.querySelector('#hour-9');
+// $('#hour-9')
+ //console.log(moment().format("l"))
+
+
+    //var currentDay = document.querySelector('#currentDay');
+    // currentDay.textContent = moment().format("l");
+
+    $("#currentDay").text(moment().format("l"));
+
+    // Event listener for Save Button
+
+        //Save text area content to local storage
+
+    
+
+    //Local storage get item to pull saved data from local storage
+
+
+
+
+    
+
+    // color code text area based on past/present/future
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
